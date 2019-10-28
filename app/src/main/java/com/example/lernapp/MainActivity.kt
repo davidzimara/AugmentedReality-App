067310ff4f5c0a3b102f6bacf2aee0ca3b541880
davidzimara <david.zimara@web.de>
2019-10-28 14:24:46 +0100

    plugin

--- app/src/main/java/com/example/lernapp/MainActivity.kt
+++ app/src/main/java/com/example/lernapp/MainActivity.kt
@@ -2,18 +2,27 @@ package com.example.lernapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
     }
+
+
 
 }
