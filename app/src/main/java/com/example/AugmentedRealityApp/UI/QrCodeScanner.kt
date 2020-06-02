d0f42dd528c92398a87099314816c15ac4e2e112
David Zimara <david.zimara@infoserve.endress.com>
2020-06-02 16:48:46 +0200

    removed last visited

--- app/src/main/java/com/example/AugmentedRealityApp/UI/QrCodeScanner.kt
+++ app/src/main/java/com/example/AugmentedRealityApp/UI/QrCodeScanner.kt
@@ -18,6 +18,7 @@ class  QrCodeScanner : AppCompatActivity() {
             scanner.initiateScan()
         }
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK) {
             val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
